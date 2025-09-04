48dc1acddceb558b9155c87c3f18a294c46e74c0
aditya_A <asquare2425@gmail.com>
2025-09-04 17:51:05 +0530

    adding some more programs of collection framework

---
+++ javaProjects/src/collectionFramework/Program7.java
@@ -0,0 +1,22 @@
+package collectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program7 {
+	public static void main(String[] args) {
+		List<Integer> l1 = new ArrayList<>();
+		l1.add(10);
+		l1.add(12);
+		l1.add(18);
+		l1.add(15);
+		l1.add(17);
+		l1.add(10);
+		l1.add(12);
+		System.out.println(l1);
+		System.out.println("======= Accessing ArrayList using forEach loop =======");
+		for(Integer i : l1) {
+			System.out.println(i);
+		}
+	}
+}
